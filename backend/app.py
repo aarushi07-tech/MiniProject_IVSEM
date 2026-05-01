9a08cfcffda760b9dbb6df0d779359c118b01e1f
aarushi07-tech <aarushi.sharma_cs.aiml24@gla.ac.in>
2026-05-01 10:57:51 +0530

    backend changes

--- backend/app.py
+++ backend/app.py
@@ -11,45 +11,27 @@ CORS(app)
 def serve_frontend():
     return send_from_directory(app.static_folder, "index.html")
 
-app = Flask(__name__)
-CORS(app)
-
-@app.route("/")
-def home():
-    return jsonify({
-        "message": "Causal Representation Learning Backend Running"
-    })
-
 @app.route("/predict", methods=["POST"])
 def predict():
 
     data = request.json
 
-    study_hours = float(data["study_hours"])
-    sleep_hours = float(data["sleep_hours"])
-    attendance = float(data["attendance"])
-    internet_usage = float(data["internet_usage"])
-    stress_level = float(data["stress_level"])
-
     features = [[
-        study_hours,
-        sleep_hours,
-        attendance,
-        internet_usage,
-        stress_level
+        float(data["study_hours"]),
+        float(data["sleep_hours"]),
+        float(data["attendance"]),
+        float(data["internet_usage"]),
+        float(data["stress_level"])
     ]]
 
     prediction, probability = predict_student(features)
 
     result = "PASS" if prediction == 1 else "FAIL"
 
-    response = {
+    return jsonify({
         "prediction": result,
         "probability": round(probability * 100, 2)
-    }
-
-    return jsonify(response)
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
-
